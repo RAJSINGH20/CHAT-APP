4df25282e5ab44cbee96fdd53849b3d2c613832c
Raj Singh <rajsinghdurgapur26@gmail.com>
2026-02-01 22:21:31 +0530

    minor change on backend url

--- Frontend/src/lib/Axios.js
+++ Frontend/src/lib/Axios.js
@@ -4,7 +4,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 
 export const axiosInstance = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: `${BACKEND_URL}/api`,
   withCredentials: true,
   
 });
