10818b76b15b12337553adccaaebbc846c1d2302
Raj Singh <rajsinghdurgapur26@gmail.com>
2026-02-01 23:21:27 +0530

    fixed the cors error

--- Backend/index.js
+++ Backend/index.js
@@ -14,12 +14,13 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CORS_ORIGIN,
     credentials: true,
   })
 );
