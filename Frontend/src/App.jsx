5d007fd7b0562acb3e938e075221bbe7c271494a
Raj Singh <rajsinghdurgapur26@gmail.com>
2026-02-01 22:24:40 +0530

    fixed the deploy issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,12 +7,12 @@ import SettingsPage from "./Pages/Setting";
 import ProfilePage from "./Pages/Profile";
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/UseAuthStore.js";
 import useThemeStore from "./Store/useThemeStore.js";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/UseAuthStore.js";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
