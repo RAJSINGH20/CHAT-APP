924c853929d1cdad9f0bb19f309aa463f41cfe8b
Raj Singh <rajsinghdurgapur26@gmail.com>
2026-02-01 23:08:59 +0530

    added components

---
+++ Frontend/src/components/ChatContanier.jsx
@@ -0,0 +1,104 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
+
+import ChatHeader from "./ChatHeader";
+import { formatMessageTime } from "../lib/utils";
+import { useAuthStore } from "../store/useAuthStore";
+import MessageInput from "../components/MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
+const ChatContainer = () => {
+  const {
+    message,
+    getMessages,
+    isMessagesLoading,
+    selectedUsers,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessages(selectedUsers._id);
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [
+    selectedUsers._id,
+    getMessages,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  ]);
+
+  useEffect(() => {
+    if (
+      messageEndRef.current &&
+      message &&
+      Array.isArray(message) &&
+      message.length > 0
+    ) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [message]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader/>
+        <MessageSkeleton/>
+        <MessageInput/>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {message.map((msg, index) => (
+          <div
+            key={msg._id || `${msg.senderId}-${msg.createdAt}`}
+            className={`chat ${
+              msg.senderId === authUser._id ? "chat-end" : "chat-start"
+            }`}
+            ref={index === message.length - 1 ? messageEndRef : null} // only last message gets the ref
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    msg.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUsers.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(msg.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {msg.image && (
+                <img
+                  src={msg.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {msg.text && <p>{msg.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <MessageInput />
+    </div>
+  );
+};
+export default ChatContainer;
---
+++ Frontend/src/components/ChatHeader.jsx
@@ -0,0 +1,37 @@
+import { X } from "lucide-react";
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
+
+function ChatHeader() {
+  const { selectedUsers, setselectedUsers } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+
+  return (
+    <div className="p-2.5 border-b border-base-300">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar">
+            <div className="size-10 rounded-full relative">
+              <img src={selectedUsers.profilePic || "/avatar.png"} alt={selectedUsers.fullName} />
+            </div>
+          </div>
+
+          {/* User info */}
+          <div>
+            <h3 className="font-medium">{selectedUsers.fullName}</h3>
+            <p className="text-sm text-base-content/70">
+              {onlineUsers.includes(selectedUsers._id) ? "Online" : "Offline"}
+            </p>
+          </div>
+        </div>
+
+        {/* Close button */}
+        <button onClick={() => setselectedUsers(null)}>
+          <X />
+        </button>
+      </div>
+    </div>
+  );
+}
+export default ChatHeader;
